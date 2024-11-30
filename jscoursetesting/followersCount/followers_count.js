fd03f3338f51a36bbb191fc276ca2c526f4853a9
Harisha M R <harishamr@example.com>
2024-11-30 18:51:42 -0500

    followers count reset  changes

--- jscoursetesting/followersCount/followers_count.js
+++ jscoursetesting/followersCount/followers_count.js
@@ -16,3 +16,8 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
   }
+
+  function resetCount() {
+    count=0;
+    displayCount(); // Display the count
+  }
