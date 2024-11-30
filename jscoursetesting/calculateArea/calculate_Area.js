a9a3c4e9f5556621f730c51203e9bb29309c6ea1
Harisha M R <harishamr@example.com>
2024-11-30 18:23:36 -0500

    grocery amount calculation changes

--- jscoursetesting/calculateArea/calculate_Area.js
+++ jscoursetesting/calculateArea/calculate_Area.js
@@ -7,3 +7,13 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+
+   function calculateGroceryCost() {
+    let grocery1cost = parseFloat(document.getElementById('cost1').value);
+    let grocery2cost = parseFloat(document.getElementById('cost2').value);
+    let grocery3cost = parseFloat(document.getElementById('cost3').value);
+
+    let totalcost = grocery1cost + grocery2cost + grocery3cost;
+    document.getElementById('groceryResult').innerText = `The total cost of the Grocery is : ${totalcost}`;
+
+   }
